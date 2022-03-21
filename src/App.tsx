fa24242b65a6868e762ca642d91575d586482958
Rafal <rafal.krohne@hotmail.co.uk>
2022-03-21 13:39:44 +0100

    logging cycles, adding input search component, storing origianl data

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,25 @@ interface Monster {
   name: string;
 }
 
-interface MonsterProps {
+interface MonsterSearchProps {
   monsters: Monster[];
+  searchField: string;
 }
 
 interface AppProps {}
 
-class App extends Component<AppProps, MonsterProps> {
+class App extends Component<AppProps, MonsterSearchProps> {
   constructor(props: AppProps) {
     super(props);
 
     this.state = {
+      searchField: "",
       monsters: [],
     };
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -35,9 +38,25 @@ class App extends Component<AppProps, MonsterProps> {
   }
 
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
+    console.log("render");
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
